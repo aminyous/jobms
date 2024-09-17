680345eb9b9ecfa221a43ece60bc912fc5200dd2
Amine Hichri <amyn.hichri@gmail.com>
2024-09-17 17:54:57 +0100

    Add Open Feign

---
+++ src/main/java/com/hichinfo/jobms/job/clients/CompanyClient.java
@@ -0,0 +1,12 @@
+package com.hichinfo.jobms.job.clients;
+
+import com.hichinfo.jobms.job.external.Company;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "COMPANY-SERVICE")
+public interface CompanyClient {
+    @GetMapping("/companies/{id}")
+    Company getCompany(@PathVariable("id") Long id);
+}
--- src/main/java/com/hichinfo/jobms/job/impl/JobServiceImpl.java
+++ src/main/java/com/hichinfo/jobms/job/impl/JobServiceImpl.java
@@ -4,6 +4,8 @@ package com.hichinfo.jobms.job.impl;
 import com.hichinfo.jobms.job.Job;
 import com.hichinfo.jobms.job.JobRepository;
 import com.hichinfo.jobms.job.JobService;
+import com.hichinfo.jobms.job.clients.CompanyClient;
+import com.hichinfo.jobms.job.clients.ReviewClient;
 import com.hichinfo.jobms.job.dto.JobDTO;
 import com.hichinfo.jobms.job.external.Company;
 import com.hichinfo.jobms.job.external.Review;
@@ -27,8 +29,16 @@ public class JobServiceImpl implements JobService {
     @Autowired
     RestTemplate restTemplate;
 
-    public JobServiceImpl(JobRepository jobRepository) {
+    private CompanyClient companyClient;
+    private ReviewClient reviewClient;
+
+    public JobServiceImpl(JobRepository jobRepository,
+                          CompanyClient companyClient,
+                          ReviewClient reviewClient) {
+
         this.jobRepository = jobRepository;
+        this.companyClient = companyClient;
+        this.reviewClient = reviewClient;
     }
 
 
@@ -47,15 +57,24 @@ public class JobServiceImpl implements JobService {
 
         //RestTemplate restTemplate = new RestTemplate();
 
+        // This one is using rest template
+        // Company company = restTemplate.getForObject("http://COMPANY-SERVICE:8086/companies/" + job.getCompanyId(), Company.class);
 
-        Company company = restTemplate.getForObject("http://COMPANY-SERVICE:8086/companies/" + job.getCompanyId(), Company.class);
-        ResponseEntity<List<Review>> reviewResponse = restTemplate.exchange("http://REVIEW-SERVICE:8087/reviews?companyId=" + job.getCompanyId(),
-                HttpMethod.GET,
-                null,
-                new ParameterizedTypeReference<List<Review>>() {
-                });
+        // This one is using Open Feign
+        Company company = companyClient.getCompany(job.getCompanyId());
 
-        List<Review> reviews = reviewResponse.getBody();
+        // This one is using rest template
+//        ResponseEntity<List<Review>> reviewResponse = restTemplate.exchange("http://REVIEW-SERVICE:8087/reviews?companyId=" + job.getCompanyId(),
+//                HttpMethod.GET,
+//                null,
+//                new ParameterizedTypeReference<List<Review>>() {
+//                });
+
+        // List<Review> reviews = reviewResponse.getBody();
+
+        List<Review> reviews = reviewClient.getReviews(job.getCompanyId());
+
+
 
         JobDTO jobDTO = JobMapper.mapToJobCompanyDto(
                 job, company, reviews
